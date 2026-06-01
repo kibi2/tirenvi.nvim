fdf2a76a9682dac1b7c830e55d4d171ac63424bc
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-06-01 09:33:00 +0900

    refactor: add Document._tir

--- lua/tirenvi/core/document.lua
+++ lua/tirenvi/core/document.lua
@@ -12,6 +12,7 @@ local M = {}
 -- constants / defaults
 
 ---@class Document
+---@field _tir boolean
 ---@field attr Attr_doc
 ---@field blocks Blocks
 
@@ -103,6 +104,7 @@ end
 function M.new_tirdoc(ndjsons, allow_plain)
     local tirdoc = new(ndjsons, allow_plain)
     Blocks.from_flat(tirdoc.blocks)
+    tirdoc._tir = true
     return tirdoc
 end
 
@@ -114,86 +116,100 @@ end
 function M.new_bufdoc(records, allow_plain, attrs, first)
     local bufdoc = new(records, allow_plain, attrs)
     set_attr_range(bufdoc, first)
+    bufdoc._tir = false
     return bufdoc
 end
 
----@param self Document
+---@param bufdoc Document
 ---@param no_normalize boolean|nil  -- If true, skip nomalizing.
 -- Prevents line count changes that would break put(); used for repair.
 ---@return Document
-function M.from_bufdoc(self, no_normalize)
+function M.from_bufdoc(bufdoc, no_normalize)
+    log.assert(not bufdoc._tir)
     no_normalize = no_normalize or false
-    local doc = vim.deepcopy(self)
-    Blocks.from_buf(doc.blocks, no_normalize)
-    return doc
+    local tirdoc = vim.deepcopy(bufdoc)
+    Blocks.from_buf(tirdoc.blocks, no_normalize)
+    tirdoc._tir = true
+    return tirdoc
 end
 
----@param self Document
+---@param tirdoc Document
 ---@return Document
-function M:to_tirdoc()
-    Blocks.to_flat(self.blocks)
-    return self
+function M.to_tirdoc(tirdoc)
+    log.assert(tirdoc._tir)
+    Blocks.to_flat(tirdoc.blocks)
+    tirdoc._tir = true
+    return tirdoc
 end
 
 ---@param tirdoc Document
 ---@return Document
 function M.to_bufdoc(tirdoc)
+    log.assert(tirdoc._tir)
     local bufdoc = vim.deepcopy(tirdoc)
     Blocks.to_bufdoc(bufdoc.blocks)
+    bufdoc._tir = false
     return bufdoc
 end
 
----@param self Document
+---@param bufdoc Document
 ---@return Ndjson[]
-function M:serialize()
-    return Blocks.serialize(self.blocks)
+function M.serialize(bufdoc)
+    log.assert(not bufdoc._tir)
+    return Blocks.serialize(bufdoc.blocks)
 end
 
----@param self Document
+---@param tirdoc Document
 ---@return Ndjson[]
-function M:serialize_to_flat()
+function M.serialize_to_flat(tirdoc)
+    log.assert(tirdoc._tir)
     local ndjsons = { new_attr_file() }
-    util.extend(ndjsons, Blocks.serialize(self.blocks))
+    util.extend(ndjsons, Blocks.serialize(tirdoc.blocks))
     return ndjsons
 end
 
----@param self Document
+---@param bufdoc Document
 ---@param range Range
 ---@param chached_attrs Attr[]
 ---@return Attr[]
-function M:replace_attrs(range, chached_attrs)
+function M.replace_attrs(bufdoc, range, chached_attrs)
+    log.assert(not bufdoc._tir)
     local first = Range.to_lua(range)
-    set_attr_range(self, first)
-    local doc_attrs = Blocks.collect_attrs(self.blocks)
+    set_attr_range(bufdoc, first)
+    local doc_attrs = Blocks.collect_attrs(bufdoc.blocks)
     if not chached_attrs or Range.is_whole(range) then
         return doc_attrs
     end
     return Attrs.replace_attrs(chached_attrs, range, doc_attrs)
 end
 
----@param self Document
-function M:infer_consistent_attr()
-    Blocks.infer_consistent_attr(self.blocks)
+---@param bufdoc Document
+function M.infer_consistent_attr(bufdoc)
+    log.assert(not bufdoc._tir)
+    Blocks.infer_consistent_attr(bufdoc.blocks)
 end
 
----@param self Document
-function M:set_auto_attr()
-    Blocks.set_auto_attr(self.blocks)
-    log.watch("ATTR", M.debug_attrs(self, "[5]AUTO ATTR:"))
+---@param bufdoc Document
+function M.set_auto_attr(bufdoc)
+    log.assert(not bufdoc._tir)
+    Blocks.set_auto_attr(bufdoc.blocks)
+    log.watch("ATTR", M.debug_attrs(bufdoc, "[5]AUTO ATTR:"))
 end
 
----@param self Document
+---@param bufdoc Document
 ---@param attrs Attr[]
-function M:apply_attrs_by_range(attrs)
-    Blocks.apply_attrs_by_range(self.blocks, attrs)
+function M.apply_attrs_by_range(bufdoc, attrs)
+    log.assert(not bufdoc._tir)
+    Blocks.apply_attrs_by_range(bufdoc.blocks, attrs)
 end
 
----@param self Document
+---@param tirdoc Document
 ---@param c_attrs Attr[]
-function M:apply_attrs_by_id(c_attrs)
+function M.apply_attrs_by_id(tirdoc, c_attrs)
+    log.assert(tirdoc._tir)
     local attrs_grid = Attrs.get_grid_attrs(c_attrs)
     local iblock = 0
-    for _, block in ipairs(self.blocks) do
+    for _, block in ipairs(tirdoc.blocks) do
         block.attr = Attr[block.kind].new()
         if block.kind == CONST.KIND.GRID then
             iblock = iblock + 1
@@ -213,28 +229,31 @@ function M:debug_attrs(title)
     return Attrs.debug_attrs(attrs, title)
 end
 
----@param self Document
+---@param bufdoc Document
 ---@param attrs Attr[]
 ---@param range3 Range3
-function M:inherit_neighbor_attr(attrs, range3)
-    if #self.blocks == 0 then
+function M.inherit_neighbor_attr(bufdoc, attrs, range3)
+    log.assert(not bufdoc._tir)
+    if #bufdoc.blocks == 0 then
         return
     end
-    local block = self.blocks[1]
+    local block = bufdoc.blocks[1]
     Block[block.kind].inherit_neighbor_attr(block, attrs, range3.first - 1)
-    block = self.blocks[#self.blocks]
+    block = bufdoc.blocks[#bufdoc.blocks]
     Block[block.kind].inherit_neighbor_attr(block, attrs, range3.last + 1)
 end
 
----@param self Document
-function M:insert_empty_lines()
-    Blocks.insert_empty_lines(self.blocks)
+---@param bufdoc Document
+function M.insert_empty_lines(bufdoc)
+    log.assert(not bufdoc._tir)
+    Blocks.insert_empty_lines(bufdoc.blocks)
 end
 
----@param self Document
+---@param bufdoc Document
 ---@return boolean
-function M:has_width()
-    return Blocks.has_width(self.blocks)
+function M.has_width(bufdoc)
+    log.assert(not bufdoc._tir)
+    return Blocks.has_width(bufdoc.blocks)
 end
 
 return M
