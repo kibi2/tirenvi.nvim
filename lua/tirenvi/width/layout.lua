e856a70cd9b7f458accf1cc77e290669dfbb4519
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-06-10 17:07:40 +0900

    feat: implemete Tir width auto

--- lua/tirenvi/width/layout.lua
+++ lua/tirenvi/width/layout.lua
@@ -10,28 +10,54 @@ local M        = {}
 -- Private helpers
 -----------------------------------------------------------------------
 
+---@param tirdoc Document
+local function fix(tirdoc)
+    Document.set_auto_attr(tirdoc)
+end
+
+---@param tirdoc Document
+local function max(tirdoc)
+    Document.set_auto_attr(tirdoc)
+end
+
+---@param tirdoc Document
+---@param ratio number|nil
+local function fit(tirdoc, ratio)
+    ratio = ratio or math.huge
+    local size = buffer.get_text_width()
+    for _, block in ipairs(tirdoc.blocks) do
+        if block.kind == "grid" then
+            local ncol = block.attr.columns and #block.attr.columns or #block.records
+            local width = math.floor((size - ncol - 1) / ncol)
+            for _, column in ipairs(block.attr.columns or {}) do
+                local auto = math.floor(math.max(column.width, 1) * ratio)
+                column.width = math.min(width, auto)
+            end
+        end
+    end
+end
+
+---@param tirdoc Document
+local function auto(tirdoc)
+    max(tirdoc)
+    fit(tirdoc, 1.5)
+end
+
 -----------------------------------------------------------------------
 -- Public API
 -----------------------------------------------------------------------
 
+---@param width_mode  WidthModeState
+---@param tirdoc Document
 function M.compute(width_mode, tirdoc)
     if width_mode.mode == "fix" then
-        Document.set_auto_attr(tirdoc)
+        fix(tirdoc)
     elseif width_mode.mode == "max" then
-        Document.set_auto_attr(tirdoc)
-    elseif width_mode.mode == "auto" then
-        Document.set_auto_attr(tirdoc)
+        max(tirdoc)
     elseif width_mode.mode == "fit" then
-        local size = buffer.get_text_width()
-        for _, block in ipairs(tirdoc.blocks) do
-            if block.kind == "grid" then
-                local ncol = block.attr.columns and #block.attr.columns or #block.records
-                local width = math.floor((size - ncol - 1) / ncol)
-                for _, column in ipairs(block.attr.columns or {}) do
-                    column.width = width
-                end
-            end
-        end
+        fit(tirdoc)
+    elseif width_mode.mode == "auto" then
+        auto(tirdoc)
     end
 end
 
