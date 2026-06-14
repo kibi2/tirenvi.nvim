a4780cddd852f36c4554826cd00fc4f628b02634
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-06-14 10:35:05 +0900

    refactor: rename win_width -> max_size

--- lua/tirenvi/width/layout.lua
+++ lua/tirenvi/width/layout.lua
@@ -56,17 +56,17 @@ local function max(tirdoc)
 end
 
 ---@param block Block
----@param win_width integer
+---@param max_size integer
 ---@return integer
-local function get_size(block, win_width)
+local function get_size(block, max_size)
     local columns = block.attr.columns
     local ncol = columns and #columns or #block.records
-    return win_width - ncol - 1
+    return max_size - ncol - 1
 end
 
 ---@param block Block
----@param win_width integer
-local function fit_auto_block(block, win_width)
+---@param max_size integer
+local function fit_auto_block(block, max_size)
     local total = 0
     local logws = {}
     for _, column in ipairs(block.attr.columns) do
@@ -76,7 +76,7 @@ local function fit_auto_block(block, win_width)
         total = total + logw
     end
     local columns = block.attr.columns
-    local size = get_size(block, win_width)
+    local size = get_size(block, max_size)
     for icol = 1, #columns do
         columns[icol].width = math.max(math.ceil(size * logws[icol] / total), Cell.MIN_WIDTH)
     end
@@ -99,11 +99,11 @@ local function shrink_to_fit(attr, size)
 end
 
 ---@param block Block
----@param win_width integer
-local function fit_block(block, win_width)
+---@param max_size integer
+local function fit_block(block, max_size)
     local total = Attr.get_total_width(block.attr)
     local columns = block.attr.columns
-    local size = get_size(block, win_width)
+    local size = get_size(block, max_size)
     for _, column in ipairs(columns or {}) do
         column.width = math.max(math.ceil(size * column.width / total), Cell.MIN_WIDTH)
     end
@@ -115,11 +115,11 @@ end
 local function fit(tirdoc, width_mode)
     local pages = width_mode.number[1] or 1
     local width = width_mode.number[2] or buffer.get_win_width()
-    local win_width = pages * width
+    local max_size = pages * width
     Document.set_max_attr(tirdoc)
     for _, block in ipairs(tirdoc.blocks) do
         if block.kind == "grid" then
-            fit_block(block, win_width)
+            fit_block(block, max_size)
             log.watch("ATTR", Document.debug_attrs(tirdoc, "[88]MODE"))
         end
     end
@@ -135,47 +135,47 @@ end
 
 ---@param column Attr_column
 ---@param max Attr_column
----@param win_width integer
-local function auto_expand(column, max, win_width)
+---@param max_size integer
+local function auto_expand(column, max, max_size)
     local width = math.ceil(math.sqrt(max.width * 3 / 2))
     if width <= column.width then
         return
     end
-    column.width = math.min(width, math.floor(win_width / 4))
+    column.width = math.min(width, math.floor(max_size / 4))
 end
 
 ---@param column Attr_column
 ---@param max Attr_column
----@param win_width integer
+---@param max_size integer
 ---@return boolean
-local function auto_attr(column, max, win_width)
+local function auto_attr(column, max, max_size)
     if max.width < column.width then
         auto_shrink(column, max)
         return false
     else
-        auto_expand(column, max, win_width)
+        auto_expand(column, max, max_size)
         return true
     end
 end
 
 ---@param block Block
----@param win_width integer
-local function auto_block(block, win_width)
+---@param max_size integer
+local function auto_block(block, max_size)
     local max_columns = vim.deepcopy(block.attr.columns)
-    fit_auto_block(block, win_width)
+    fit_auto_block(block, max_size)
     local is_wrap = false
     for icol = 1, #max_columns do
-        is_wrap = is_wrap or auto_attr(block.attr.columns[icol], max_columns[icol], win_width)
+        is_wrap = is_wrap or auto_attr(block.attr.columns[icol], max_columns[icol], max_size)
     end
 end
 
 ---@param tirdoc Document
 local function auto(tirdoc)
     Document.set_max_attr(tirdoc)
-    local win_width = buffer.get_win_width()
+    local max_size = buffer.get_win_width()
     for _, block in ipairs(tirdoc.blocks) do
         if block.kind == "grid" then
-            auto_block(block, win_width)
+            auto_block(block, max_size)
         end
     end
 end
