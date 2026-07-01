d673d5d474bdabec0bd3c0aac8ec19576ad62b3f
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-07-01 15:40:37 +0900

    test: update test case checkhealth

--- tests/cases/check/health-ng/run.sh
+++ tests/cases/check/health-ng/run.sh
@@ -3,5 +3,10 @@ set -eu
 
 NVIM_TIRENVI_DEV=1 nvim --headless -u NONE -n -S run.vim > stdout.txt 2> stderr.txt
 
-LC_ALL=C sort out-actual.txt > gen.txt
+LC_ALL=C sed \
+  -e 's/❌ //g' \
+  -e 's/✅ //g' \
+  -e 's/⚠️ //g' out-actual.txt | \
+LC_ALL=C sort > gen.txt
+
 mv gen.txt out-actual.txt
